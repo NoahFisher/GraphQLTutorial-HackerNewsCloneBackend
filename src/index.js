0cae1a7dea57ad45eb4d7e0a297013422ec81293
Noah Fisher <noah.fisher@gmail.com>
2019-01-21 12:05:31 -0700

    checkpoint

--- src/index.js
+++ src/index.js
@@ -1,46 +1,26 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
-
-let idCount = links.length
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    link: (parent, args) => links.find((item) => item.id === args.id)
-  },
-  Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      }
-      links.push(link)
-      return link
-    },
-    updateLink: (parent, args) => {
-      let link = links.find((item) => item.id === args.id)
-      let { description, url } = args
-      link.description = description
-      link.url = url
-
-      return link
-    }
-    deleteLink: (parent, args) => {
-      //TODO
-      return link
-    }
-  }
+  Query,
+  Mutation,
+  User,
+  Link
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: request => {
+    return {
+      ...request,
+      prisma
+    }
+  }
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
 
